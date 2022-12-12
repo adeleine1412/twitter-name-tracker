d0c87ee025441c359cc9d827af6f42bf5b8f8f41
adeleine1412 <53810001+adeleine1412@users.noreply.github.com>
2022-12-12 10:11:35 +0100

    Initial commit

---
+++ data/index.js
@@ -0,0 +1,46 @@
+const TelegramBot = require('node-telegram-bot-api');
+const request = require('request');
+const cheerio = require('cheerio');
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: true});
+
+const desiredUsername = process.env.DESIRED_USERNAME;
+let announceUnavailable = false;
+
+bot.on('message', function(message) {
+  console.log(message);
+
+  if (message.text != 'check') return;
+  announceUnavailable = true;
+  checkUsername();
+});
+
+const options = {
+  url: `https://nitter.net/${desiredUsername}`,
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+  }
+};
+
+function checkUsername() {
+  request(options, function(error, response, html) {
+    if (error) {
+      bot.sendMessage('357187163', `Error checking availability of username ${desiredUsername}: ${error}`);
+    } else {
+      const $ = cheerio.load(html);
+  
+      if ($('span').text().includes(`User "${desiredUsername}" not found`)) {
+        bot.sendMessage('357187163', `Username ${desiredUsername} is now available.`);
+      } else {
+        console.log(`Username ${desiredUsername} is not available.`);
+
+        if (!announceUnavailable) return;
+        announceUnavailable = false;
+        bot.sendMessage('357187163', `Username ${desiredUsername} is not available.`);
+      }
+    }
+  });
+}
+
+setInterval(function() { checkUsername() }, 300000);
+checkUsername();
